500b85eb4c0d00f9bce02e905be2bd3a066e1212
TrpFrog <dev@trpfrog.net>
2024-05-04 17:29:21 +0900

    Add cleanup

--- my-app/app/switch-demo/page.tsx
+++ my-app/app/switch-demo/page.tsx
@@ -4,7 +4,11 @@ import { useState, useEffect } from "react";
 export default function SwitchDemo() {
   const [pushed, setPushed] = useState(false);
   useEffect(() => {
+    const originalTitle = document.title;
     document.title = `現在の状態: ${pushed ? "ON" : "OFF"}`;
+    return () => {
+      document.title = originalTitle;
+    };
   }, [pushed]);
   return (
     <main style={{ textAlign: "center" }}>
